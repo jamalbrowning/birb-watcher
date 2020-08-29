f6c84c36ffc3f84d27cc43b543271f8233066293
Jim Browning <jamalebrowning@gmail.com>
2020-08-29 10:01:18 -0500

    created components

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import './App.scss';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
-import fbConnection from '../javascripts/helpers/data/connection';
+import fbConnection from '../helpers/data/connection';
 
-import NavBar from '../javascripts/components/pages/NavBar/NavBar';
+import Auth from '../components/pages/Auth/Auth';
+import NavBar from '../components/pages/NavBar/NavBar';
+import EditBirb from '../components/pages/EditBirb/EditBirb';
+import Home from '../components/pages/Home/Home';
+import NewBirb from '../components/pages/NewBirb/NewBirb';
+import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
 
 fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Auth />
+
+        <EditBirb />
+        <Home />
+        <NewBirb />
+        <SingleBirb />
       </div>
     );
   }
